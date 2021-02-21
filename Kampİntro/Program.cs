fc9223711195195a2e3eb42ca1b8c218665258d2
Baktemur <75330735+Baktemur@users.noreply.github.com>
2021-02-21 16:07:56 +0300

    Add files via upload
    C# Kamp Kodları

---
+++ Kampİntro/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarBugun = 7.55;
+            double dolarDun = 7.55;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+
+
+        }
+        
+	
+
+	
+    }
+}
